e5dc819b9625fe2a1dfa74d63a34bcfae4d1dc55
Pradeep-Rallapalli <pradeep.rgs@icloud.com>
2026-08-21 10:11:06 -0700

    Initial Commit

---
+++ Lift!/Lift_App.swift
@@ -0,0 +1,17 @@
+//
+//  Lift_App.swift
+//  Lift!
+//
+//  Created by Pradeep Rallapalli on 8/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Lift_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
