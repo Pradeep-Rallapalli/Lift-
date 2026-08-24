40f28fa3634b9e72e599a6b65f27686f9379edab
Pradeep-Rallapalli <pradeep.rgs@icloud.com>
2026-08-24 10:37:35 -0700

    Refactor ExerciseSheetView into its own file. Cleaned up AddWorkoutView
    Fix:
    Workout plan name gets cleared if user doesnt type anything and clicks confirm.

--- Lift!/WorkoutView/WorkoutDetailView.swift
+++ Lift!/WorkoutView/WorkoutDetailView.swift
@@ -14,12 +14,27 @@ struct WorkoutDetailView: View {
 
     var body: some View {
         NavigationStack {
-            ScrollView {
-                VStack {
-                    ForEach(workoutPlan.exercises) { exercise in
-                        ExerciseCardView(exercise: exercise)
+            VStack {
+                ScrollView {
+                    VStack {
+                        ForEach(workoutPlan.exercises) { exercise in
+                            ExerciseCardView(exercise: exercise)
+                        }
                     }
                 }
+                
+                Button {
+                    //TODO: Start workout session
+                    
+                } label: {
+                        Text("Start Workout")
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color(.secondarySystemBackground))
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .padding()
+                
             }.navigationTitle(workoutPlan.planName).toolbarTitleDisplayMode(.inline)
         }
     }
@@ -28,3 +43,12 @@ struct WorkoutDetailView: View {
 #Preview {
     WorkoutDetailView(workoutPlan: PreviewData.planA)
 }
+/*
+ 
+ 1. User clicks on exercise.
+ 2. Sheet view opens with pre populated fields.
+ 3. User updates exercise
+ 4. Persistant store updates that specific exercise with new values
+ 
+ 
+ */
